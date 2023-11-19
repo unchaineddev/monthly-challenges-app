c930017abf3d32a92465b9c0518d9dcd75db9c30
yusuf <yusuftechie+git@gmail.com>
2023-11-19 11:45:29 +0530

    added dynamic urls

--- challenge/urls.py
+++ challenge/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.first_view),  # views.function_name
-    path("february", views.second_view)
+    # path("january", views.first_view),  # views.function_name
+    # path("february", views.second_view),
+    # path("march", views.march),
+    path("<month>", views.my_monthly_challenge )  # irrespective of what url we pass this will trigger
 ]
--- challenge/views.py
+++ challenge/views.py
@@ -1,11 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseNotFound  #for 404 pages
 
 # Create your views here.
 
 # first view
-def first_view(request):
-    return HttpResponse("Work out!")
+# def first_view(request):
+#     return HttpResponse("Work out!")
 
-def second_view(request):
-    return HttpResponse("Eat Healthy!")
+# def second_view(request):
+#     return HttpResponse("Eat Healthy!")
+
+
+# def march(request):
+#     return HttpResponse("Go for a Jog!")
+    
+
+def my_monthly_challenge(request, month):  # month is passed in url i.e  <month>
+    text = None
+    
+    if month == 'january':
+        text = "Work out!"
+    elif month == 'february':
+        text = "Eat Healthy!"
+    elif month == 'march':
+        text = "Go for a Walk!"
+    elif month == 'april':
+        text = "Play a sport!"
+    elif month == 'may':
+        text = "May the Job Search begin!"
+    else:
+        return HttpResponseNotFound("404 PAGE NOT FOUND!")
+
+    return HttpResponse(text)
