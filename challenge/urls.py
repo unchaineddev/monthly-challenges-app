fa3d36c81b92acf181ff6f7ced511b3a0185c564
yusuf <yusuftechie+git@gmail.com>
2023-11-19 11:22:55 +0530

    added comments for first view & added urls

--- challenge/urls.py
+++ challenge/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.first_view)
+    path("january", views.first_view)  # views.function_name
 ]
--- monthly_challenge/urls.py
+++ monthly_challenge/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
+from django.urls import include   # import this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("challenge/", include("challenge.urls"))
+    path("challenge/", include("challenge.urls"))  # path(app_name, include("app_name.urls") 
 ]
