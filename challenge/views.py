b9ecc85f14e1b95a6d794e134d4a25953a7dfa4c
yusuf <yusuftechie+git@gmail.com>
2023-11-19 11:20:25 +0530

    created first view & added urls

--- challenge/views.py
+++ challenge/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+# first view
+def first_view(request):
+    return HttpResponse("This is working!")
