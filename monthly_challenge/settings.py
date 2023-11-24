c2273a6530f03aaa156267988b0ebb27ddee8f76
yusuf <yusuftechie+git@gmail.com>
2023-11-24 12:23:17 +0530

    adding css & font

--- monthly_challenge/settings.py
+++ monthly_challenge/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',  # handle static files
 ]
 
 MIDDLEWARE = [
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'   # which url serves static files
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
